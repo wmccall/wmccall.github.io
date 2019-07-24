a421c3d4001652f499e2bf8c29ed8764e56387ac
William McCall <dev@willmccall.com>
2019-07-23 23:16:14 -0400

    Update achievement

--- src/app/constants/achievements.js
+++ src/app/constants/achievements.js
@@ -1,6 +1,6 @@
 const ACHIEVEMENTS_WORDS = [
   'RIT_Presidential_Scholarship',
-  '{DeansList:[2015,2016,2017,2018]}',
+  '{DeansList:[2015,2016,2017,2018,2019]}',
 ];
 
 const achievements = ACHIEVEMENTS_WORDS.map(word => ({
