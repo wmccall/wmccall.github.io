57088cd5277a48a5c0c1f80229a594c481222d1a
Will McCall <wnm.code@gmail.com>
2019-08-19 17:59:17 -0400

    Change frequence of background code

--- src/app/constants/backgroundCode.js
+++ src/app/constants/backgroundCode.js
@@ -6,7 +6,7 @@ const backgroundCode = words => {
   const allCode = [];
   let wordCount = 0;
   for (let codeCount = 0; wordCount < words.length; codeCount += 1) {
-    if (codeCount % 13 === 0) {
+    if (codeCount % 17 === 0) {
       allCode.push(
         pseudoCode.pseudoImport(words[wordCount], {
           type: 'none',
@@ -15,18 +15,18 @@ const backgroundCode = words => {
       );
       wordCount += 1;
     }
-    if (codeCount % 11 === 0) {
+    if (codeCount % 18 === 0) {
       allCode.push(pseudoCode.pseudoMethodCall(words[wordCount]));
       wordCount += 1;
     }
     // filler below
-    if (codeCount % 3 === 0) {
+    if (codeCount % 13 === 0) {
       allCode.push(pseudoCode.pseudoReturnMethod());
     }
-    if (codeCount % 4 === 0) {
+    if (codeCount % 15 === 0) {
       allCode.push(pseudoCode.pseudoReturnString());
     }
-    if (codeCount % 10 === 0) {
+    if (codeCount % 17 === 0) {
       allCode.push(pseudoCode.pseudoStringConstDefinition());
     }
   }
