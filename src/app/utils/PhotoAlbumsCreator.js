5a651210d9dc735d6fa939a8d720dec1fcfa6825
William McCall <wnm.code@gmail.com>
2019-09-16 14:34:12 -0400

    Add sizes to photoshop photos

--- src/app/utils/PhotoAlbumsCreator.js
+++ src/app/utils/PhotoAlbumsCreator.js
@@ -1,14 +1,15 @@
 export default albums =>
   albums.map(album => {
     const newPhotoElements = album.photoElements.map(
-      ({ src, caption, thumbnailWidth, thumbnailHeight }) => {
-        const thumbnail =
-          src.slice(0, src.length - 4) + 't' + src.slice(src.length - 4);
+      ({ src, caption, tw, th }) => {
+        const thumbnail = `${src.slice(0, src.length - 4)}t${src.slice(
+          src.length - 4,
+        )}`;
         return {
           src,
-          thumbnail: thumbnail,
-          thumbnailWidth: thumbnailWidth || 0,
-          thumbnailHeight: thumbnailHeight || 0,
+          thumbnail,
+          thumbnailWidth: tw || 0,
+          thumbnailHeight: th || 0,
           caption,
         };
       },
