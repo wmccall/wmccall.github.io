2c940876adefad7f9ed7c4b95f2ad77d3f792016
Will McCall <wnm.code@gmail.com>
2019-09-17 20:41:56 -0400

    Restructure site, add build contents to root (#2)

---
+++ precache-manifest.558c3276759633e7778a0d78270ff706.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9904f208c3b9de90b4a9622fbfe405c",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "12296add7c60cc189ccf",
+    "url": "/website/static/css/main.b0381f84.chunk.css"
+  },
+  {
+    "revision": "dd86bd27cb76b144f44c",
+    "url": "/website/static/js/2.a17e45fa.chunk.js"
+  },
+  {
+    "revision": "12296add7c60cc189ccf",
+    "url": "/website/static/js/main.3bae000f.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "bc04c26940e56591762bd93ad389a941",
+    "url": "/website/static/media/BulkPing.bc04c269.png"
+  },
+  {
+    "revision": "ca68d9dba9eaf2583b5bf06fb8c3d815",
+    "url": "/website/static/media/ClearDownloadsRepeat.ca68d9db.png"
+  },
+  {
+    "revision": "4162b014236b1471745857a54e77fce5",
+    "url": "/website/static/media/DroidSansMono-webfont.4162b014.eot"
+  },
+  {
+    "revision": "8e99c48b441442d16804d0b17277a74e",
+    "url": "/website/static/media/DroidSansMono-webfont.8e99c48b.ttf"
+  },
+  {
+    "revision": "9f0a7088b68ffaf8929fa7af9cdc2057",
+    "url": "/website/static/media/DroidSansMono-webfont.9f0a7088.svg"
+  },
+  {
+    "revision": "a91b581da1321170f5cc29d17b82644f",
+    "url": "/website/static/media/DroidSansMono-webfont.a91b581d.woff"
+  },
+  {
+    "revision": "37b3bb8a136c1b0d208ee2b17ec92625",
+    "url": "/website/static/media/WillKeeb-min.37b3bb8a.jpg"
+  }
+]);
