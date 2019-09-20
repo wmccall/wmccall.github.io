f15da771fe1d1bc60cb83bf058c16b7a7ff58d8e
William McCall <wnm.code@gmail.com>
2019-09-20 10:38:57 -0400

    Sort words by tag, update blur css to account for larger displays

--- Source/src/app/constants/allWords.js
+++ Source/src/app/constants/allWords.js
@@ -3,11 +3,11 @@ import interests from './interests';
 import skills from './skills';
 
 const allWords = () =>
-  [...projects, ...interests, ...skills].sort((a, b) => {
-    if (a.word.toLowerCase() < b.word.toLowerCase()) {
+  [...skills, ...projects, ...interests].sort((a, b) => {
+    if (a.tag.toLowerCase() < b.tag.toLowerCase()) {
       return -1;
     }
-    if (a.word.toLowerCase() > b.word.toLowerCase()) {
+    if (a.tag.toLowerCase() > b.tag.toLowerCase()) {
       return 1;
     }
     return 0;
