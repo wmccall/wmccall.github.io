68e8d22e62caea89f7ddbbb668cbac7db479b912
William McCall <wnm.code@gmail.com>
2019-09-22 19:30:18 -0400

    Uncomment photos

--- Source/src/app/constants/mechKeebPhotoElements.js
+++ Source/src/app/constants/mechKeebPhotoElements.js
@@ -1,53 +1,53 @@
 import photoAlbumsCreator from '../utils/PhotoAlbumsCreator';
 
 const ALBUMS = [
-  // {
-  //   title: 'Tux and Frost',
-  //   photoElements: [
-  //     {
-  //       src: 'https://imgur.com/Bmd2IWP.jpg',
-  //       caption: 'Starting the Build',
-  //       tw: 4640,
-  //       th: 3480,
-  //     },
-  //     {
-  //       src: 'https://imgur.com/0QhapEh.jpg',
-  //       caption: 'Placing the Clear Switches',
-  //       tw: 4640,
-  //       th: 3480,
-  //     },
-  //     {
-  //       src: 'https://imgur.com/WJE6VF2.jpg',
-  //       caption: 'Placing the Brown Switches',
-  //       tw: 4640,
-  //       th: 3480,
-  //     },
-  //     {
-  //       src: 'https://imgur.com/08NrPhn.jpg',
-  //       caption: 'Soldering',
-  //       tw: 4640,
-  //       th: 3480,
-  //     },
-  //     {
-  //       src: 'https://imgur.com/UEY3uKg.jpg',
-  //       caption: 'Finshed Keyboards',
-  //       tw: 4640,
-  //       th: 3480,
-  //     },
-  //     {
-  //       src: 'https://imgur.com/reBKTrr.jpg',
-  //       caption: 'Tux',
-  //       tw: 4640,
-  //       th: 3480,
-  //     },
-  //     {
-  //       src: 'https://imgur.com/ByFumyi.jpg',
-  //       caption: 'Frost',
-  //       tw: 4640,
-  //       th: 3480,
-  //     },
-  //   ],
-  // },
+  {
+    title: 'Tux and Frost',
+    photoElements: [
+      {
+        src: 'https://imgur.com/Bmd2IWP.jpg',
+        caption: 'Starting the Build',
+        tw: 4640,
+        th: 3480,
+      },
+      {
+        src: 'https://imgur.com/0QhapEh.jpg',
+        caption: 'Placing the Clear Switches',
+        tw: 4640,
+        th: 3480,
+      },
+      {
+        src: 'https://imgur.com/WJE6VF2.jpg',
+        caption: 'Placing the Brown Switches',
+        tw: 4640,
+        th: 3480,
+      },
+      {
+        src: 'https://imgur.com/08NrPhn.jpg',
+        caption: 'Soldering',
+        tw: 4640,
+        th: 3480,
+      },
+      {
+        src: 'https://imgur.com/UEY3uKg.jpg',
+        caption: 'Finshed Keyboards',
+        tw: 4640,
+        th: 3480,
+      },
+      {
+        src: 'https://imgur.com/reBKTrr.jpg',
+        caption: 'Tux',
+        tw: 4640,
+        th: 3480,
+      },
+      {
+        src: 'https://imgur.com/ByFumyi.jpg',
+        caption: 'Frost',
+        tw: 4640,
+        th: 3480,
+      },
+    ],
+  },
   {
     title: 'Mint',
     photoElements: [
